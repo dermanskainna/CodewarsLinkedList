4d96ec70536e426cccd51f76fb110d8ea758a8b3
dermanskainna <dermanskainna@gmail.com>
2026-03-21 18:16:19 +0200

    Зроблено завдання Linked Lists - Remove Duplicates

---
+++ Linked Lists - Remove Duplicates/task_8.py
@@ -0,0 +1,22 @@
+'''
+remove duplicate
+'''
+class Node(object):
+    '''
+    node
+    '''
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    '''
+    remove same
+    '''
+    current = head
+    while current is not None and current.next is not None:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
